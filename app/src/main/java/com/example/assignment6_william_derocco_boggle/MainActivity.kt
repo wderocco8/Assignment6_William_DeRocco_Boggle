5cc17de28cb97737ce03015bd3d0d8b32be28f94
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-03-29 12:20:52 -0400

    restricted portrait mode and removed boilerplate

--- app/src/main/java/com/example/assignment6_william_derocco_boggle/MainActivity.kt
+++ app/src/main/java/com/example/assignment6_william_derocco_boggle/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.assignment6_william_derocco_boggle
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Lock orientation to portrait
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
     }
 }
